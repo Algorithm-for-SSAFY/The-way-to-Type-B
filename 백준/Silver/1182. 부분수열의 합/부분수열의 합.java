93507219222b7d797507693859ac044046225724
aabccde <112850083+aabccde@users.noreply.github.com>
2026-02-25 15:19:56 +0900

    [Silver II] Title: 부분수열의 합, Time: 96 ms, Memory: 11904 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
@@ -1,57 +1,41 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 
-
-import java.io.*;
-import java.util.*;
-
-
-public class Main  {
+public class Main {
+	static List<Integer> seq = new ArrayList<>();
+	static int res = 0;
+	static int N;
+	static int S;
+	static List<Integer> seqP = new ArrayList<>();
 	
-	static int n;
-	static int s;
-	static int[] input;
-	static int cnt;
-	static boolean[] visited;
-	
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		n = Integer.parseInt(st.nextToken());
-		s = Integer.parseInt(st.nextToken());
-		input = new int[n];
-		visited= new boolean[n];
-		cnt = 0;
-             		
-		st = new StringTokenizer(br.readLine());
-		
-		for (int i = 0; i<n; i++) {
-			input[i] = Integer.parseInt(st.nextToken());
-			if (input[i] == s) {
-				cnt++;
-			}
-		}
-		
-		for (int i = 2; i<=n; i++) {
-			dfs(0, 0, i, 0);
-		}
-		System.out.println(cnt);
-		
-	}
-	
-	public static void dfs(int depth, int start, int maxD,int val) {
-		if (depth == maxD) {
-			if (val == s) {
-				cnt++;
-			}
+	public static void dfsS(int idx, int cSum) {
+		if (idx == N) {
+			if(cSum == S) res++;
 			return;
 		}
 		
-		for (int i = start; i<n; i++) {
-			if (!visited[i]) {
-				visited[i] = true;
-				int tmpVal = val + input[i];
-				dfs(depth+1,i, maxD, tmpVal);
-				visited[i] = false;
-			}
+		dfsS(idx+1,cSum+seq.get(idx));
+		dfsS(idx+1,cSum);
+	}
+
+	public static void main(String args[]) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());// N개의 정수
+		S = Integer.parseInt(st.nextToken());// 다 더한 값
+
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			seq.add(Integer.parseInt(st.nextToken()));
 		}
+		dfsS(0, 0);
+		if(S==0) {
+			res--;
+		}
+
+		System.out.println(res);
 	}
 }
