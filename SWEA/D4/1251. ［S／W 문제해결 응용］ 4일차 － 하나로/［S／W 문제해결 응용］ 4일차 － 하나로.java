0e940394e45d2c687f9eb59266f29c7a5eb9fd3b
aabccde <112850083+aabccde@users.noreply.github.com>
2026-03-12 11:10:49 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 하나로, Time: 201 ms, Memory: 82,752 KB -BaekjoonHub

--- SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
+++ SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
@@ -3,35 +3,9 @@ import java.io.*;
 
 public class Solution {
 	static int N;
-	static int[][] islandLocations;
-
-	static class Island {
-		int index;
-		long x;
-		long y;
-
-		public Island(int index, long x, long y) {
-			this.index = index;
-			this.x = x;
-			this.y = y;
-		}
-	}
-
-	static class Connection {
-		Island is1;
-		Island is2;
-		double distance;
-
-		public Connection(Island is1, Island is2) {
-			this.is1 = is1;
-			this.is2 = is2;
-			this.distance = Math.pow(is1.x - is2.x, 2) + Math.pow(is1.y - is2.y, 2);
-		}
-	}
-
-	static int[] arr;
-	static Island[] islands;
 	static double E;
+	static int[][] islands;
+	static int[] roots;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -40,74 +14,100 @@ public class Solution {
 		int T = Integer.parseInt(br.readLine());
 		for (int testCase = 1; testCase <= T; testCase++) {
 			N = Integer.parseInt(br.readLine());
-			islandLocations = new int[N][2];
+			islands = new int[N][2];
+			roots = new int[N];
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			for (int i = 0; i < N; i++) {
-				islandLocations[i][0] = Integer.parseInt(st.nextToken());
+				islands[i][0] = Integer.parseInt(st.nextToken());
 			}
 			st = new StringTokenizer(br.readLine());
 			for (int i = 0; i < N; i++) {
-				islandLocations[i][1] = Integer.parseInt(st.nextToken());
+				islands[i][1] = Integer.parseInt(st.nextToken());
+				roots[i] = i;
 			}
 			E = Double.parseDouble(br.readLine());
-			islands = new Island[N];
-			for (int i = 0; i < N; i++) {
-				islands[i] = new Island(i, islandLocations[i][0], islandLocations[i][1]);
-			}
-			PriorityQueue<Connection> pq = new PriorityQueue<>(new Comparator<Connection>() {
-				@Override
-				public int compare(Connection c1, Connection c2) {
-					if (c1.distance < c2.distance) {
-						return -1;
-					} else {
-						return 1;
-					}
-				}
-			});
-			for (int i = 0; i < N; i++) {
-				for (int j = i + 1; j < N; j++) {
-					pq.offer(new Connection(islands[i], islands[j]));
-				}
-			}
-			arr = new int[N];
-			for (int i = 0; i < N; i++) {
-				arr[i] = i;
-			}
-			int count = 0;
-			double distSum = 0;
-			while (true) {
-				if (count == N - 1) {
-					break;
-				}
-				Connection c = pq.poll();
-				if (union(c.is1.index, c.is2.index)) {
-					distSum += c.distance;
-					count++;
-				}
-			}
-			long answer = Math.round(E * distSum);
-			sb.append("#").append(testCase).append(" ").append(answer).append("\n");
+			double answer = Math.round(PRIM());
+			sb.append("#").append(testCase).append(" ").append((long) answer).append("\n");
 		}
 		bw.write(sb.toString());
 		bw.flush();
 	}
 
+	static class Tunnel {
+		int num1;
+		int num2;
+		double cost;
+
+		public Tunnel(int num1, int num2) {
+			this.num1 = num1;
+			this.num2 = num2;
+			this.cost = getCost(num1, num2);
+		}
+	}
+
+	public static double getCost(int num1, int num2) {
+		return E * (Math.pow(islands[num1][0] - islands[num2][0], 2)
+				+ Math.pow(islands[num1][1] - islands[num2][1], 2));
+	}
+
+	public static double PRIM() {
+		boolean[] visited = new boolean[N];
+		int visitedCount = 0;
+		double sum = 0.0;
+		PriorityQueue<Tunnel> pq = new PriorityQueue<>(new Comparator<Tunnel>() {
+			@Override
+			public int compare(Tunnel t1, Tunnel t2) {
+				return Double.compare(t1.cost, t2.cost);
+			}
+		});
+		for (int i = 1; i < N; i++) {
+			pq.offer(new Tunnel(0, i));
+		}
+		visited[0] = true;
+		visitedCount++;
+		while (!pq.isEmpty()) {
+			Tunnel t = pq.poll();
+			int num1 = t.num1;
+			int num2 = t.num2;
+			if(visited[num2]) {
+				continue;
+			}
+			if (union(num1, num2)) {
+				sum += t.cost;
+				visited[num2] = true;
+				visitedCount++;
+				if (visitedCount == N) {
+					return sum;
+				}
+				for (int i = 0; i < N; i++) {
+					if (num2 == i) {
+						continue;
+					}
+					if (!visited[i]) {
+						pq.offer(new Tunnel(num2, i));
+					}
+				}
+			}
+		}
+		return sum;
+	}
+
 	public static boolean union(int num1, int num2) {
-		if (getRoot(num1) == getRoot(num2)) {
+		if (roots[num1] == roots[num2]) {
 			return false;
 		}
-		arr[getRoot(num1)] = getRoot(num2);
+		roots[roots[num1]] = roots[num2];
 		return true;
 	}
 
-	public static int getRoot(int num) {
+	public static int find(int num) {
 		List<Integer> list = new ArrayList<>();
-		while (arr[num] != num) {
+		while (roots[num] != num) {
 			list.add(num);
-			num = arr[num];
+			num = roots[num];
 		}
 		for (int i : list) {
-			arr[i] = num;
+			roots[i] = num;
 		}
 		return num;
 	}
