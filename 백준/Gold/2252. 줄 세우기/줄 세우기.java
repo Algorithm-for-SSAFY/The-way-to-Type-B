4f8d25da6c12f5ad3f3086e6b2cca39848739726
aabccde <112850083+aabccde@users.noreply.github.com>
2026-03-23 15:14:28 +0900

    [Gold III] Title: 줄 세우기, Time: 372 ms, Memory: 46340 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -2,66 +2,51 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	
-	static List<List<Integer>> Lst = new ArrayList<>();
-	static int[] indegree;
-	static int N,M;
-	
-	
+	static int N, M;
+	static List<Integer>[] connectedTo;
+	static int[] preCount;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		StringBuilder sb = new StringBuilder();
-		
-		st = new StringTokenizer(br.readLine());
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-		indegree = new int[N + 1];
-		
-		for (int i = 0;i<=N; i++) {
-			Lst.add(new ArrayList<>());
+		connectedTo = new List[N+1];
+		preCount = new int[N+1];
+		for(int i=1; i<=N; i++) {
+			connectedTo[i] = new ArrayList<>();
 		}
-		
-		for (int i = 0;i<M; i++) {
+		for(int i=0; i<M; i++) {
 			st = new StringTokenizer(br.readLine());
-			int start = Integer.parseInt(st.nextToken());
-			int end = Integer.parseInt(st.nextToken());
-			Lst.get(start).add(end);
-			indegree[end]++;
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			connectedTo[a].add(b);
+			preCount[b]++;
 		}
-		
-		Queue<Integer> q = new ArrayDeque<>();
-		List<Integer> result = new ArrayList<>();
-		
-		for (int i = 1;i<=N; i++) {
-			if (indegree[i] == 0) {
-				q.offer(i);
+		StringBuilder sb = BFS(new StringBuilder());
+		bw.write(sb.toString());
+		bw.flush();
+	}
+	public static StringBuilder BFS(StringBuilder sb) {
+		boolean[] visited = new boolean[N+1];
+		Queue<Integer> queue = new LinkedList<>();
+		for(int i=1; i<=N; i++) {
+			if(preCount[i] == 0) {
+				queue.offer(i);
+				visited[i] = true;
 			}
 		}
-		
-		while (!q.isEmpty()) {
-			int idx = q.poll();
-			result.add(idx);
-			
-			for(int next : Lst.get(idx)) {
-				indegree[next]--;
-				
-				if (indegree[next] == 0) {
-					q.offer(next);
+		while(!queue.isEmpty()) {
+			int current = queue.poll();
+			sb.append(current).append(" ");
+			for(int next : connectedTo[current]) {
+				preCount[next]--;
+				if(preCount[next] == 0 && !visited[next]) {
+					queue.offer(next);
+					visited[next] = true;
 				}
 			}
 		}
-		
-		if (result.size() != N) {
-			System.out.println(-1);
-		} else {
-			for(int idx : result) {
-				System.out.print(idx+" ");
-			}
-		}
-		
-		
-		
+		return sb;
 	}
-
 }
