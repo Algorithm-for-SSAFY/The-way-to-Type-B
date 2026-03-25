670c6d5335f6ad320c47e909ca59e18e04767cc0
aabccde <112850083+aabccde@users.noreply.github.com>
2026-03-25 14:15:07 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 1168 ms, Memory: 211720 KB -BaekjoonHub

--- 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
@@ -11,33 +11,21 @@ public class Main {
 		arr = new int[N];
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		arr[0] = Integer.parseInt(st.nextToken());
-		List<Integer> list = new ArrayList<>();
-		list.add(arr[0]);
+		TreeSet<Integer> ts = new TreeSet<>();
+		ts.add(arr[0]);
 		for(int i=1; i<N; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
 			int num = arr[i];
-			if(num > list.get(list.size()-1)) {
-				list.add(num);
+			if(ts.last() < num) {
+				ts.add(num);
 			} else {
-				int replacedIndex = findIndex(list, num);
-				list.set(replacedIndex, num);
+				int a = ts.ceiling(num);
+				ts.remove(a);
+				ts.add(num);
 			}
 		}
-		int answer = list.size();
+		int answer = ts.size();
 		bw.write(answer + "\n");
 		bw.flush();
-	}
-	public static int findIndex(List<Integer> list, int num) {
-		int left = 0;
-		int right = list.size()-1;
-		while(left < right) {
-			int middle = (left + right) / 2;
-			if(list.get(middle) < num) {
-				left = middle + 1;
-			} else {
-				right = middle;
-			}
-		}
-		return left;
 	}
 }
